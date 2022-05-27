1b3dc1b3f6f664c7a7cb19898be8520a45bfe1a0
iamstevol <94203327+iamstevol@users.noreply.github.com>
2022-05-27 22:31:22 +0100

    Update zureal1.py

--- zureal1.py
+++ zureal1.py
@@ -9,14 +9,3 @@ def find_anagrams(str1, str2):
 str1 = input("type in a word: ")
 str2 = input("type in a word: ")
 find_anagrams(str1, str2)
-
-def find_anagrams(str1, str2):
-    # [assignment] Add your code here
-    if (sorted(str1) == sorted(str2)):
-        print("True")
-    else:
-        print("False")
-
-str1 = input("type in a word: ")
-str2 = input("type in a word: ")
-find_anagrams(str1, str2)
